b9dff48e0d94cc9ca7a2f80f73d6e8b2c0d143b2
SamuelChitturi <sambennny@gmail.com>
2023-06-09 18:44:44 +0530

    first commit

---
+++ cmail.py
@@ -0,0 +1,25 @@
+
+import smtplib
+from smtplib import SMTP
+from email.message import EmailMessage
+def sendmail(to,subject,body):
+    server=smtplib.SMTP_SSL('smtp.gmail.com',465)
+    server.login('sambennny@gmail.com','urseptlozherwlvo')
+    msg=EmailMessage()
+    msg['From']='sambennny@gmail.com'
+    msg['Subject']=subject
+    msg['To']=to
+    msg.set_content(body)
+    server.send_message(msg)
+    server.quit()
+
+    
+
+
+
+
+
+
+
+        
+    
