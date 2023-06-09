73f194e84e3ad3853599790dffc510f35b5dd2fa
SamuelChitturi <sambennny@gmail.com>
2023-06-09 19:47:26 +0530

    'n'

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import stripe
 stripe.api_key='sk_test_51NEwbpSGV8I6se9KORwedExAXFwLOMKImSP2IUR2kBUvA8lAhHEUXbPEuTKjPweb1V4Db1ij200j8PEx3Nsv5F2o00UtOfttxi'
 app=Flask(__name__)
 app.secret_key='jnjnnjskkskosiskc'
+#deployment part
 db=os.environ['RDS_DB_NAME']
 user= os.environ['RDS_USERNAME']
 password= os.environ['RDS_PASSWORD']
@@ -19,7 +20,7 @@ with mysql.connector.connect(host=host,password=password,user=user,db=db):
     cursor.execute("create table if not exists adminreg(username varchar(30) primary key,password varchar(20),email varchar(50))")
     cursor.execute("create table if not exists registration(username varchar(50) primary key,mobile varchar(20) unique,email varchar(50) unique,address varchar(50),password varchar(20))")
     cursor.execute("create table if not exists orders(oid int primary key,itemid varchar(9),itemname varchar(20),q int,total int,username varchar(30),foreign key (itemid) references additems(itemid) on update cascade on delete cascade,foreign key (username) references users(username))")
-
+#deployment part ends
 mydb=mysql.connector.connect(host='localhost',user='root',password='admin',db='samuel')
 @app.route('/',methods=['GET','POST'])
 def home():
